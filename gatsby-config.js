c7adcc2bdb9f836f727025d15464d26e945ca926
kschmitz123 <kathrin_schmitz93@gmx.de>
2021-03-13 16:09:04 +0100

    Install and add dependencies to config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,19 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
 module.exports = {
   siteMetadata: {
     title: `e.gilles_writes`,
     description: `A writer's blog`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 }
